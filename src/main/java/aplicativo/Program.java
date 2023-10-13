7091bab49d5e3f61a9c32f33001ffdfbe3b27676
TSKsteff <steff@edu.univali.br>
2023-10-13 19:57:42 -0300

    Segundo commit (Pegando do banco)

--- src/main/java/aplicativo/Program.java
+++ src/main/java/aplicativo/Program.java
@@ -11,22 +11,16 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Pessoa p1 = new Pessoa(1,"Ritz", "ritz@gmail.com");
-		Pessoa p2 = new Pessoa( 2,"Ted", "ted@gmail.com");
-		Pessoa p3 = new Pessoa(3,"Steff", "steff@gmail.com");
-		Pessoa p4 = new Pessoa( 4,"Piard", "piard@gmail.com");
+
 		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
-		
-		em.getTransaction().begin();
-		em.persist(p1);
-		em.persist(p2);
-		em.persist(p3);
-		em.persist(p4);
-		em.getTransaction().commit();
-		
-		
-		
+
+		Pessoa p = em.find(Pessoa.class, 2);
+		System.out.println(p);
+
+		System.out.println("Pronto!");
+		em.close();
+		emf.close();
 	}
 }
