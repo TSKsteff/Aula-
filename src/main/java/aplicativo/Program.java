d6198827de66bbac82521381ab253750d1d9e540
TSKsteff <steff@edu.univali.br>
2023-10-13 00:54:46 -0300

    Primeiro commit (Inserindo no banco)

---
+++ src/main/java/aplicativo/Program.java
@@ -0,0 +1,32 @@
+package aplicativo;
+
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import dominio.Pessoa;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Pessoa p1 = new Pessoa(1,"Ritz", "ritz@gmail.com");
+		Pessoa p2 = new Pessoa( 2,"Ted", "ted@gmail.com");
+		Pessoa p3 = new Pessoa(3,"Steff", "steff@gmail.com");
+		Pessoa p4 = new Pessoa( 4,"Piard", "piard@gmail.com");
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		em.getTransaction().begin();
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		em.persist(p4);
+		em.getTransaction().commit();
+		
+		
+		
+	}
+}
