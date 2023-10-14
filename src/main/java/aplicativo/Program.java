d64854900496b70013c42effa30e459e34194d05
TSKsteff <steff@edu.univali.br>
2023-10-14 01:39:13 -0300

    Terceiro commit Removendo da base

--- src/main/java/aplicativo/Program.java
+++ src/main/java/aplicativo/Program.java
@@ -10,17 +10,34 @@ import dominio.Pessoa;
 public class Program {
 
 	public static void main(String[] args) {
-		
 
-		
+		/**
+		Pessoa p1 = new Pessoa(1,"Ritz", "ritz@gmail.com");
+		Pessoa p2 = new Pessoa( 2,"Ted", "ted@gmail.com");
+		Pessoa p3 = new Pessoa(3,"Steff", "steff@gmail.com");
+		Pessoa p4 = new Pessoa( 4,"Piard", "piard@gmail.com");
+		*/
+
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
 
-		Pessoa p = em.find(Pessoa.class, 2);
-		System.out.println(p);
+		/**
+		em.getTransaction().begin();
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		em.persist(p4);
+		em.getTransaction().commit();*/
 
-		System.out.println("Pronto!");
+		Pessoa p = em.find(Pessoa.class, 2);
+		em.getTransaction().begin();
+		em.remove(p);
+		em.getTransaction().commit();
+		System.out.println(p);
+		System.out.println("Pronto");
 		em.close();
 		emf.close();
+		
+		
 	}
 }
